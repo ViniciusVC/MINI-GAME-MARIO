d39be99e52152aeab35ea0e5782faf3d6bc9310d
ViniciusVC <vinivalent@gmail.com>
2024-08-10 16:50:02 -0300

    Mais personagens

--- MARIO/script.js
+++ MARIO/script.js
@@ -1,37 +1,66 @@
 const mario = document.querySelector('.mario')
 const pipe = document.querySelector('.pipe')
+const menu = document.querySelector('.menu')
 
 const start = document.querySelector('.start')
 const gameOver = document.querySelector('.game-over')
 
-audioStart = new Audio('./src/audio/audio_theme.mp3')
-audioGameOver = new Audio('./src/audio/audio_gameover.mp3')
 
 
-const startGame = () => {
+var choiceUniv = "mario"
+
+const universe = {
+  "mario": {
+    "avatar" : "./img/mario.gif",
+    "gameover" : "./img/game-over.png",
+    "enemy" : "./img/pipe.png",
+    "audioStart" : new Audio('./soung/audio_theme.mp3'),
+    "audioGameOver" : new Audio('./soung/audio_gameover.mp3')
+  },
+  "sonic":{
+    "avatar" : "./img/gif_sonic_correndo.gif",
+    "gameover" : "./img/sonic_a.png",
+    "enemy" : "./img/robotnik_a.png",
+    "audioStart" : new Audio('./soung/sonicinicio1.m4a'),
+    "audioGameOver" : new Audio('./soung/sonicmorre.m4a')
+  },
+  "mickey":{
+    "avatar" : "./img/mickey_run.gif",
+    "gameover" : "./img/mickey_dano.png",
+    "enemy" : "./img/terreno_mikey.png",
+    "audioStart" : new Audio('./soung/audio_theme.mp3'),
+    "audioGameOver" : new Audio('./soung/audio_gameover.mp3')
+  }
+}
+
+
+
+const startGame = (varChoice) => {
+
+  choiceUniv = varChoice;
+  mario.src = universe[choiceUniv].avatar;
+  pipe.src = universe[choiceUniv].enemy;
   pipe.classList.add('pipe-animation')
-  start.style.display = 'none'
+  
+  //start.style.display = 'none'
+  menu.style.display = 'none'
 
   // audio
-  audioStart.play()
+  universe[choiceUniv].audioStart.play()
 }
 
 const restartGame = () => {
-  gameOver.style.display = 'none'
-  pipe.style.left = ''
-  pipe.style.right = '0'
-  mario.src = './src/img/mario.gif'
-  mario.style.width = '150px'
-  mario.style.bottom = '0'
-
-  start.style.display = 'none'
-
-  audioGameOver.pause()
-  audioGameOver.currentTime = 0;
-
-  audioStart.play()
-  audioStart.currentTime = 0;
-
+  gameOver.style.display = 'none';
+  pipe.style.left = '';
+  pipe.style.right = '0';
+  mario.src = universe[choiceUniv].avatar;
+  mario.style.width = '150px';
+  mario.style.bottom = '0';
+  start.style.display = 'none';
+  universe[choiceUniv].audioGameOver.pause()
+  universe[choiceUniv].audioGameOver.currentTime = 0;
+  universe[choiceUniv].audioStart.play()
+  universe[choiceUniv].audioStart.currentTime = 0;
 }
 
 const jump = () => {
@@ -50,26 +79,23 @@ const loop = () => {
       .bottom.replace('px', ' ')
 
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-      pipe.classList.remove('.pipe-animation')
-      pipe.style.left = `${pipePosition}px`
-
-      mario.classList.remove('.jump')
-      mario.style.bottom = `${marioPosition}px`
-
-      mario.src = './src/img/game-over.png'
-      mario.style.width = '80px'
-      mario.style.marginLeft = '50px'
-      
+      pipe.classList.remove('.pipe-animation');
+      pipe.style.left = `${pipePosition}px`;
+      mario.classList.remove('.jump');
+      mario.style.bottom = `${marioPosition}px`;
+      mario.src = universe[choiceUniv].gameover;
+      mario.style.width = '80px';
+      mario.style.marginLeft = '50px';    
       
       function stopAudioStart() {
-        audioStart.pause()
+        universe[choiceUniv].audioStart.pause()
       }
       stopAudioStart()
       
-      audioGameOver.play()
+      universe[choiceUniv].audioGameOver.play()
       
       function stopAudio() {
-        audioGameOver.pause()
+        universe[choiceUniv].audioGameOver.pause()
       }
       setTimeout(stopAudio, 7000)
       
